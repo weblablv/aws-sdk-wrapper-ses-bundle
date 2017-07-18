e7a338be240e877bbd89e30d268da712eace852f
Edvards Umbrass <edvards.umbrass@gmail.com>
2017-07-18 10:26:02 +0000

    return container extension in bundle class

--- src/AmazonSdkSesWrapperBundle/WebLabLvAmazonSdkSesWrapperBundle.php
+++ src/AmazonSdkSesWrapperBundle/WebLabLvAmazonSdkSesWrapperBundle.php
@@ -3,5 +3,17 @@
 namespace WebLabLv\Bundle\AmazonSdkSesWrapperBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
-final class WebLabLvAmazonSdkSesWrapperBundle extends Bundle {}
+use WebLabLv\Bundle\AmazonSdkSesWrapperBundle\DependencyInjection\WebLabLvAmazonSdkSesWrapperExtension;
+
+final class WebLabLvAmazonSdkSesWrapperBundle extends Bundle
+{
+	/**
+	 * @return Extension
+	 */
+	public function getContainerExtension(): Extension
+	{
+		return new WebLabLvAmazonSdkSesWrapperExtension();
+	}
+}
